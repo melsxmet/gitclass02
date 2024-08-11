939d98fb6f3ae8b632ee7b45e422fb01b02dfe7b
melsxmet <mellisaouttrim@gmail.com>
2024-08-11 18:01:54 -0400

    3rd commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,10 +2,8 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("This is the project version 1");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("This is the project version 5");
+
     }
 }
-//done with version 2
+//done with version 3
