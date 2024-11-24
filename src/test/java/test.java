04c02beb3a45597cc536298e4ef17cc5df5f2fe2
melsxmet <mellisaouttrim@gmail.com>
2024-11-24 14:46:39 -0500

    sixth commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("This is the project version 5");
+        System.out.println("This is the project version 6");
 
     }
 }
