943a089f339df8cda05b4838fbc607c1c99ebc22
melsxmet <mellisaouttrim@gmail.com>
2024-11-24 15:02:16 -0500

    person2commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,7 +2,7 @@ public class test {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("This is the project version 6");
+        System.out.println("This is the project version 6 made by person 2");
 
     }
 }
